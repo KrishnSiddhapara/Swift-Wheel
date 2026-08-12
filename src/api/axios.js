2da74de1d627effc598e4b35be841b120c73ce75
KrishnSiddhapara <siddhaparakishan@gmail.com>
2026-08-12 20:14:51 +0530

    Fix URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_ORIGIN || 'http://localhost:5000'}/api`,
+   baseURL: 'https://swift-wheel-backend.vercel.app/api',
 });
 
 // Add a request interceptor to include the JWT token
